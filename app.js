cf9a47c8d68994be020741ed23a4f536b715f72a
titouan <titouanok@outlok.com>
2025-01-12 17:07:45 +0100

    page web

---
+++ app.js
@@ -0,0 +1,38 @@
+import * as sc from "./script.js";
+
+const app = document.getElementById("app");
+
+let NomUtilisateur = "";
+
+if (NomUtilisateur === "") {
+  app.innerHTML = `
+                <h1>Bienvenue !</h1>
+                <form id="welcome-form">
+                    <input
+                    type="text"
+                    id="username"
+                    placeholder="Entrez votre nom d'utilisateur"
+                    required
+                    />
+                    <button type="submit">Entrer</button>
+                </form>
+                `;
+
+  const form = document.getElementById("welcome-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    NomUtilisateur = document.getElementById("username").value;
+    if (NomUtilisateur !== "") {
+      loadMainPage();
+    }
+  });
+}
+function loadMainPage() {
+  NomUtilisateur = sc.getUsername();
+  app.innerHTML = `
+        <h1>Bienvenue, ${NomUtilisateur} !</h1>
+        
+    `;
+}
