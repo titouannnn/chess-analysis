ded3fc62b63ddb42788878ae312e69f6ed613d0c
titouannnn <titouanmok@outlook.com>
2025-01-22 18:44:33 +0100

    je suis pas fan d'angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Api } from './api.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: '<div>{{ message }}</div>'
 })
-export class AppComponent {
-  title = 'WEB';
+
+export class AppComponent implements OnInit {
+  message: string = ''; // Variable pour afficher le résultat
+
+  constructor(private api: Api) {} // Injection du service
+
+  ngOnInit(): void {
+    console.log("Initialisation de l'application");
+    this.message = this.api.getUsername(""); // Appel de la fonction getUsername
+    this.initializeSettings();
+  }
+  
+
+  async initializeSettings(): Promise<void> {
+    console.log("Récupération des parties en ligne");
+    console.log("user : " + this.api.username);
+    this.api.getAllGamesOFFLINE();
+    /*
+    this.api.sortByGameType(this.api.RAPID);
+    this.api.initTimeInterval();
+    this.api.setTimeTinterval(this.api.YEAR,this.api.DATENULL,this.api.DATENULL);
+    console.log(this.api.allGames);
+    */
+    
+  }
 }
