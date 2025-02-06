35ca0609a89d90fcaa6af08d09893f86b888699e
Henrei3 <95311577+Henrei3@users.noreply.github.com>
2025-02-06 13:46:55 +0100

    dev: debug components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Api } from './api.service';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  template: '<div>{{ message }}</div>'
+  imports: [HomeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: "./app.component.css"
 })
 
 export class AppComponent implements OnInit {
